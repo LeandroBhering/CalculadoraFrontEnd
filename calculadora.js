d340f382927070ae0d0e7d5b1cece5b51013493a
LeandroBhering <leandrobenega@gmail.com>
2021-08-30 22:55:32 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -4,9 +4,30 @@ const display = document.getElementById('display')
 const numeros = document.querySelectorAll('[id*=tecla]')
 const operadores = document.querySelectorAll('[id*=operador]')
 
+var novoNumero = true
+var operador 
+var numeroAnterior 
+
+
 const atualizarDisplay = (texto) => {
-    display.textContent += texto
+    if (novoNumero){
+        display.textContent = texto
+        novoNumero = false
+    }else{
+        display.textContent += texto
+    }
 }
 
 const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent)
 numeros.forEach(numero => numero.addEventListener('click', inserirNumero))
+
+const selecionarOperador = (evento) => {
+    if (!novoNumero){
+        novoNumero = true
+        operador = evento.target.textContent
+        numeroAnterior = display.textContent
+        console.log(numeroAnterior, operador)
+    }
+}
+operadores.forEach(operador => operador.addEventListener('click',selecionarOperador))
+
